fb2d1a550c3eb1a74e6eeb8f7e3c98c7e0987cfc
Mark <markleager92@gmail.com>
2023-07-29 13:20:50 -0400

    add level1

---
+++ level1/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.9.0"
+    }
+  }
+  required_version = ">= 1.2.0"
+
+  backend "s3" {
+    bucket         = "tf-state-mentorship"
+    key            = "level1.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "tf-remote-state-locking"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
