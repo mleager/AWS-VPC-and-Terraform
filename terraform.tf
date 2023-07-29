b332e04fb3945f47cb9e1c1354791f82036611d2
Mark <markleager92@gmail.com>
2023-07-29 11:55:33 -0400

    run terraform fmt

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "tf-state-mentorship"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "tf-state-mentorship"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
     dynamodb_table = "tf-remote-state-locking"
   }
 }
