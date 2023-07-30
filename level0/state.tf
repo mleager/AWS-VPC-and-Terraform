3062cafe43169915b2e5cdb84b2ea02616c13d2f
Mark <markleager92@gmail.com>
2023-07-30 13:06:19 -0400

    update DyanmoDB Table to PROVISIONED

--- level0/state.tf
+++ level0/state.tf
@@ -7,9 +7,11 @@ resource "aws_s3_bucket" "remote_state" {
 }
 
 resource "aws_dynamodb_table" "state_locking" {
-  name         = "tf-remote-state-locking"
-  hash_key     = "LockID"
-  billing_mode = "PAY_PER_REQUEST"
+  name           = "tf-remote-state-locking"
+  hash_key       = "LockID"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
 
   attribute {
     name = "LockID"
