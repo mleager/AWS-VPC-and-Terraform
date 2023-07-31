c7f8d1bf70b65d59c631e6caecaf39e58d9a5873
Mark <markleager92@gmail.com>
2023-07-30 20:24:53 -0400

    add ALB and components, modify AZs and Security Groups in instance.tf

--- level2/instance.tf
+++ level2/instance.tf
@@ -14,14 +14,14 @@ data "aws_ami" "amazonlinux2" {
   owners = ["amazon"] # "137112412989"
 }
 
-resource "aws_instance" "public_webserver" {
-  ami                         = data.aws_ami.amazonlinux2.id
-  instance_type               = var.instance_type
-  associate_public_ip_address = true
-  iam_instance_profile        = var.iam_instance_profile
-  key_name                    = var.key_name
-  subnet_id                   = data.terraform_remote_state.level1.outputs.public_subnet_id[0]
-  user_data = file("user_data.sh")
+resource "aws_instance" "webserver_1" {
+  ami           = data.aws_ami.amazonlinux2.id
+  instance_type = var.instance_type
+  #associate_public_ip_address = true
+  iam_instance_profile = var.iam_instance_profile
+  key_name             = var.key_name
+  subnet_id            = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
+  user_data            = file("user_data.sh")
 
   vpc_security_group_ids = [aws_security_group.public.id]
 
@@ -36,7 +36,7 @@ resource "aws_security_group" "public" {
   vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
 
   ingress {
-    description = "SSH from public"
+    description = "SSH from Local Machine"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -48,7 +48,8 @@ resource "aws_security_group" "public" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    #cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.alb-sg.id]
   }
 
   egress {
@@ -63,14 +64,14 @@ resource "aws_security_group" "public" {
   }
 }
 
-resource "aws_instance" "private_webserver" {
+resource "aws_instance" "webserver_2" {
   ami                  = data.aws_ami.amazonlinux2.id
   instance_type        = var.instance_type
   iam_instance_profile = var.iam_instance_profile
   key_name             = var.key_name
-  subnet_id            = data.terraform_remote_state.level1.outputs.private_subnet_id[0]
+  subnet_id            = data.terraform_remote_state.level1.outputs.private_subnet_id[1]
 
-  vpc_security_group_ids = [aws_security_group.private.id]
+  vpc_security_group_ids = [aws_security_group.public.id]
 
   tags = {
     Name = "${var.env_code}-private"
---
+++ level2/loadbalancer.tf
@@ -0,0 +1,72 @@
+resource "aws_lb" "alb" {
+  name               = "tf-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb-sg.id]
+  subnets            = [for subnet in data.terraform_remote_state.level1.outputs.public_subnet_id : subnet]
+
+  access_logs {
+    bucket  = "tf-state-mentorship" # make an output on level0?
+    prefix  = "test-lb"
+    enabled = false
+  }
+
+  tags = {
+    Name = "${var.env_code}-alb"
+  }
+}
+
+resource "aws_lb_target_group" "alb_tg" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.terraform_remote_state.level1.outputs.vpc_id
+}
+
+resource "aws_lb_target_group_attachment" "alb_attach_1" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = aws_instance.webserver_1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "alb_attach_2" {
+  target_group_arn = aws_lb_target_group.alb_tg.arn
+  target_id        = aws_instance.webserver_2.id
+  port             = 80
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_tg.arn
+  }
+}
+
+resource "aws_security_group" "alb-sg" {
+  name        = "${var.env_code}-alb-sg"
+  description = "Allow ALB access"
+  vpc_id      = data.terraform_remote_state.level1.outputs.vpc_id
+
+  ingress {
+    description = "Allow HTTP Traffic"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env_code}-alb-sg"
+  }
+}
