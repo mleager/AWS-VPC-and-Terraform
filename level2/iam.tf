7c54c25e120891b7b4d3ee5f62b65db7677feb0b
Mark <markleager92@gmail.com>
2023-07-31 21:35:35 -0400

    format iam.tf

--- level2/iam.tf
+++ level2/iam.tf
@@ -29,7 +29,7 @@ resource "aws_iam_policy" "s3_policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "s3:*",
+        Action   = "s3:*",
         Effect   = "Allow",
         Resource = "*"
       },
