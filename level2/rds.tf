f7a92e33e07eadc00543c7d891a723369397677f
Mark <markleager92@gmail.com>
2023-08-05 17:51:20 -0400

    format file

--- level2/rds.tf
+++ level2/rds.tf
@@ -4,5 +4,5 @@ module "rds" {
   env_code          = var.env_code
   vpc_id            = data.terraform_remote_state.level1.outputs.vpc_id
   private_subnet_id = data.terraform_remote_state.level1.outputs.private_subnet_id
-  asg_sg            = module.asg.asg_sg 
+  asg_sg            = module.asg.asg_sg
 }
