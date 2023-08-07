2ae5f8fda587831b43a63867c68a4b255c1da236
Mark <markleager92@gmail.com>
2023-08-07 16:28:12 -0400

    change vpc module to open source

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -2,112 +2,21 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.env_code}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = slice(data.aws_availability_zones.available.names, 0, 2)
+  private_subnets = var.private_cidr
+  public_subnets  = var.public_cidr
+
+  enable_nat_gateway = true
+  single_nat_gateway = false
 
   tags = {
-    Name = var.env_code
+    Terraform = "true"
+    Name      = "${var.env_code}-vpc"
   }
-}
-
-resource "aws_subnet" "public" {
-  count = length(var.public_cidr)
-
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.public_cidr[count.index]
-
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "${var.env_code}-public${count.index}"
-  }
-}
-
-resource "aws_subnet" "private" {
-  count = length(var.private_cidr)
-
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.private_cidr[count.index]
-
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-
-  tags = {
-    Name = "${var.env_code}-private${count.index}"
-  }
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = var.env_code
-  }
-}
-
-resource "aws_eip" "nat" {
-  count = length(var.public_cidr)
-
-  domain = "vpc"
-
-  tags = {
-    Name = "${var.env_code}-nat${count.index}"
-  }
-
-  depends_on = [aws_internet_gateway.main]
-}
-
-resource "aws_nat_gateway" "main" {
-  count = length(var.public_cidr)
-
-  allocation_id = aws_eip.nat[count.index].id
-  subnet_id     = aws_subnet.public[count.index].id
-
-  tags = {
-    Name = "nat-${count.index}"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "${var.env_code}-public"
-  }
-}
-
-resource "aws_route_table" "private" {
-  count = length(var.private_cidr)
-
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.main[count.index].id
-  }
-
-  tags = {
-    Name = "${var.env_code}-private${count.index}"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  count = length(var.public_cidr)
-
-  subnet_id      = aws_subnet.public[count.index].id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route_table_association" "private" {
-  count = length(var.private_cidr)
-
-  subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private[count.index].id
 }
